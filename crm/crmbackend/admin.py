c572c1c139d0628f3232820904e2023ec68e7b31
Айзат <Aimix444@gmail.com>
2023-10-26 00:23:10 +0300

    added houses to adminka

--- crm/crmbackend/admin.py
+++ crm/crmbackend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Users, Roles
+from .models import Users, Roles, Houses
 
 admin.site.register(Users)
 admin.site.register(Roles)
+admin.site.register(Houses)
